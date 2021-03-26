93d8b6866000dbaa96bb49aae21a56a714a60b31
Matt Pelyhes <mattpelyhes@gmail.com>
2021-03-26 17:37:57 -0500

    add save button and styling

--- client/writers-blok/src/components/Notepad.js
+++ client/writers-blok/src/components/Notepad.js
@@ -3,8 +3,11 @@ import React from 'react';
 const Notepad = () => {
   return (
     <div className="notepad">
-      <h1>Title</h1>
+      <div className="title">
+        <h1>Title</h1><span className="blinking-cursor">|</span>
+      </div>
       <textarea rows="4" cols="50" placeholder="Select a prompt to begin your writing warm-up"></textarea>
+      <button className="save-button">Save</button>
     </div>
   )
 }
