fba6aecfc7946a2b14e80fc027b6c9b53377997f
Matt Pelyhes <mattpelyhes@gmail.com>
2021-03-27 10:22:55 -0500

    Add create prompt form and styling

--- client/writers-blok/src/components/CreatePrompt.js
+++ client/writers-blok/src/components/CreatePrompt.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const CreatePrompt = () => {
   return(
-    <div>Nada</div>
+    <div className="create-prompt-container">
+      <h3>Create New Prompt</h3>
+      <form className="create-prompt-form">
+        <div>
+          <label htmlFor="title"></label>
+          <input type="text" className="create-title" name="title" placeholder="Title" />
+        </div>
+        <div >
+          <label htmlFor="prompt"></label>
+          <textarea className="create-prompt" name="prompt" rows="4" placeholder="Prompt Description" />
+        </div>
+        <button className="save-button">Save</button>
+      </form>
+    </div>
   )
 }
 
--- client/writers-blok/src/components/Main.js
+++ client/writers-blok/src/components/Main.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Prompt from './Prompt';
 import Navbar from './Navbar';
+import CreatePrompt from './CreatePrompt'
 
 const Main = () => {
   return (
     <div className="main">
       <Navbar />
+      <CreatePrompt />
+      {/* <Prompt />
       <Prompt />
       <Prompt />
       <Prompt />
       <Prompt />
-      <Prompt />
-      <Prompt />
+      <Prompt /> */}
     </div>
   )
 }
