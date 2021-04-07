6b6f5dd08aa43bc04f2664406eb70f8180d460af
Matt Pelyhes <mattpelyhes@gmail.com>
2021-04-07 17:51:54 -0500

    add likes to ui using a "pseudolike"

--- updated-client/src/components/PromptItem.js
+++ updated-client/src/components/PromptItem.js
@@ -3,11 +3,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp as liked } from "@fortawesome/free-solid-svg-icons";
 import {faThumbsUp as notLiked } from "@fortawesome/free-regular-svg-icons"
 
-const PromptItem = ({ text, title, author, onPromptSelect, selectedPrompt }) => {
+const PromptItem = ({ text, title, author, likes, onPromptSelect, selectedPrompt }) => {
   const [likeButton, setLikedButton] = useState(false);
+  const [pseudoLikes, setPseudoLikes] = useState(likes);
 
   const handleLike = () => {
-    setLikedButton(true);
+    if(likeButton){
+      setLikedButton(false)
+      setPseudoLikes( pseudoLikes - 1)
+    } else {
+      setLikedButton(true)
+      setPseudoLikes(pseudoLikes + 1)
+    }
   };
 
   return(
@@ -16,11 +23,16 @@ const PromptItem = ({ text, title, author, onPromptSelect, selectedPrompt }) => 
       <div className="prompt">
         {text || "This is where a really cool description of the prompt would go! I would put lorem ipsum text here, but I forgot how to do that quickly using emmett. Oh well!"}
       </div>
-      <div className="icon-container" onClick={handleLike}>
-        {!likeButton ? <FontAwesomeIcon icon={notLiked}  className="icon" /> : <FontAwesomeIcon icon={liked} className="icon" />}
-      </div>
-      <div className="prompt-author">
-        Submitted By <span className="colon">:</span> {author || "Matt Pelyhes"}
+      <div className="prompt-bottom">
+        <div className="prompt-author">
+          Submitted By <span className="colon">:</span> {author || "Matt Pelyhes"}
+        </div>
+        <div className="like-container">
+          <div className="icon-container" onClick={handleLike}>
+            {!likeButton ? <FontAwesomeIcon icon={notLiked}  className="icon" /> : <FontAwesomeIcon icon={liked} className="icon" />}
+          </div>
+          <div className="total-likes">{pseudoLikes}</div>
+        </div>
       </div>
     </div>
   );
