a7678afe7fd72e49bad735b0b38d6e06588aa75c
Matt Pelyhes <mattpelyhes@gmail.com>
2021-04-07 16:35:59 -0500

    add font awesom like button and styling

--- updated-client/src/components/PromptItem.js
+++ updated-client/src/components/PromptItem.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp as liked } from "@fortawesome/free-solid-svg-icons";
+import {faThumbsUp as notLiked } from "@fortawesome/free-regular-svg-icons"
 
 const PromptItem = ({ text, title, author, onPromptSelect, selectedPrompt }) => {
+  const [likeButton, setLikedButton] = useState(false);
 
+  const handleLike = () => {
+    setLikedButton(true);
+  };
 
   return(
     <div className={title === selectedPrompt ? "prompt-container prompt-selected" : "prompt-container"} onClick={() => onPromptSelect(title)}>
@@ -9,8 +16,11 @@ const PromptItem = ({ text, title, author, onPromptSelect, selectedPrompt }) => 
       <div className="prompt">
         {text || "This is where a really cool description of the prompt would go! I would put lorem ipsum text here, but I forgot how to do that quickly using emmett. Oh well!"}
       </div>
+      <div className="icon-container" onClick={handleLike}>
+        {!likeButton ? <FontAwesomeIcon icon={notLiked}  className="icon" /> : <FontAwesomeIcon icon={liked} className="icon" />}
+      </div>
       <div className="prompt-author">
-        Submitted by <span className="colon">:</span> {author || "Matt Pelyhes"}
+        Submitted By <span className="colon">:</span> {author || "Matt Pelyhes"}
       </div>
     </div>
   );
